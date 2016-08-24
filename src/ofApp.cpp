e6f0c7cfdc77d3f8a1ebb591b47b336914a2babf
Your Name <marlier.maxime@gmail.com>
2016-08-24 09:18:13 +0200

    load image, binirize and compute histogram

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,15 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofBackground(0,0,0);
+	ofSetVerticalSync(true);
+    Tresh = 255;
 
+    src_img.loadImage("images/Portrait.png");
+    cpy_img.clone(src_img);
+    rot_img.clone(cpy_img);
+	computeBinarization(&cpy_img, Tresh);
+	computeHisto(cpy_img);
 }
 
 //--------------------------------------------------------------
@@ -12,50 +20,115 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	cpy_img.draw(0, 0);
+	drawHisto(0, 600);
+}
 
+void ofApp::computeHisto(ofImage img){
+  	ofColor col_hsb;
+    int histo[255] = {0};
+
+	unsigned char *data = img.getPixels();
+    int components = 3;
+
+    int pixs = 0;
+
+	for (int x=0; x<img.width; x++) {
+		for (int y=0; y<img.height; y++) {
+	        //Read pixel (x,y) color components
+	        // int index = components * (x + x<img.width * y);
+	        // int red = data[ index ];
+	        // int green = data[ index + 1 ];
+	        // int blue = data[ index + 2 ];
+
+	        ofColor c = img.getColor(x, y);
+	        if (c.getBrightness()  == 0){
+	        	pixs++;
+	        	// getchar();
+	        }
+        }
+    	// printf("%d  - ", pixs);
+        histDat.push_back(pixs);
+        pixs = 0;
+    }
+}
+
+/**
+ * @brief Binarize an image with a specific trshold
+ * 
+ * @param img poiter to the image to modify
+ * @param tresh value of treshold for 0 to 255
+ */
+void ofApp::computeBinarization(ofImage *img, int tresh){
+
+	//Getting pointer to pixel array of tdf
+	unsigned char *data = img->getPixels();
+	//Calculate number of pixel components
+	int components = img->bpp / 8;
+	//Modify pixel array
+	for (int y=0; y<img->height; y++) {
+	    for (int x=0; x<img->width; x++) {
+
+	        //Read pixel (x,y) color components
+	        int index = components * (x + img->width * y);
+	        int gray = data[ index ];
+
+	        int bin = 0;
+
+	        if (gray > tresh)
+	        	bin = 255;
+
+	        //Set red 
+	        data[ index + RED] = bin ;
+	        //Set green 
+	        data[ index + GREEN ] = bin;
+	        //Set blue 
+	        data[ index + BLUE] = bin;
+
+	    }
+	}
+	//Calling img.update() to apply changes
+	img->update();	
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
-}
+	switch (key){
+		case 'p':
+			Tresh+=10;
+			cpy_img.clone(src_img);
+			computeBinarization(&cpy_img, Tresh);
+			printf("plus\n");
+			computeHisto(cpy_img);
+			break;
+		case 'm':	
+			Tresh-=10;
+			cpy_img.clone(src_img);
+			computeBinarization(&cpy_img, Tresh);
+			printf("moins\n");
+			computeHisto(cpy_img);
+			break;
+		default:
+			break;
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+	}
 
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+	printf("tresh = %d\n", Tresh);
 
 }
 
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+/**
+ * @brief draw the histogram from the Histodata
+ */
+void ofApp::drawHisto(int start_x, int start_y){
+	// draw a line for each elmt in the hestoData
+	int x_pos = start_x, end_y;
+    ofSetHexColor(0xffffff);
 
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+	for (std::vector<int>::iterator i = histDat.begin(); i != histDat.end(); ++i){
+    	end_y = start_y - (*i);
+    	// printf("%d\n", (*i));
+    	ofLine(x_pos++, start_y, x_pos, end_y);
+	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,11 @@
 
 #include "ofMain.h"
 
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -10,13 +15,23 @@ class ofApp : public ofBaseApp{
 		void draw();
 
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+		// void keyReleased(int key);
+		// void mouseMoved(int x, int y );
+		// void mouseDragged(int x, int y, int button);
+		// void mousePressed(int x, int y, int button);
+		// void mouseReleased(int x, int y, int button);
+		// void windowResized(int w, int h);
+		// void dragEvent(ofDragInfo dragInfo);
+		// void gotMessage(ofMessage msg);
+
+		void computeHisto(ofImage img);
+		void computeBinarization(ofImage *img, int tresh);
+		void drawHisto(int start_x, int start_y);
+
+		ofImage src_img, cpy_img, rot_img;
+		int Tresh;
+
+        std::vector<int> histDat;
+
 		
 };
